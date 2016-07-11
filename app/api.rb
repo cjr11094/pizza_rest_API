8714ab9ae5d53b71d1bf2ac7eafa04d1ce9393c3
cjr <creilly16@amherst.edu>
2016-07-11 00:35:22 -0400

    started logic for streaks and max month

--- app/api.rb
+++ app/api.rb
@@ -14,5 +14,70 @@ module Rest
             { :total => Pizza.count, :data => Pizza.all.map { |e| { :id => e.id, :person_id => e.person_id, :type => e.type, :eaten_at => e.date } } }
         end
     end
+
+    resource "streaks" do
+        get do
+            @users = Pizza.all.map { |e| { :id => e.id, :eaten_at => e.date } }
+            @users.sort_by { |hsh| hsh[:eaten_at] }
+            prevPurchaseDate = nil
+            currStreak = 0
+            prevStreak = 0
+            streaks = []
+            streak = []
+            @users.each do |hsh|
+                currPurchaseDate = hsh[:eaten_at]
+                if prev != nil
+                    if prevPurchaseDate == currPurchaseDate
+                        currStreak+=1
+                    else
+                        if currStreak >= prevStreak
+                            streak.push(prevPurchaseDate)
+                        else
+                            if streak.length > 1
+                                streaks.push(streak)
+                                streak = []
+                            end
+                        end
+                    end
+                    prevPurchaseDate = currPurchaseDate
+                else
+                    prevPurchaseDate = hsh[:eaten_at]
+                end
+            end
+        end
+    end
+
+    resource "most_pizza" do
+        get do
+            @users = Pizza.all.map { |e| { :id => e.id, :eaten_at => e.date } }
+            @users.sort_by { |hsh| hsh[:eaten_at] }
+            prevPurchaseDate = nil
+            currStreak = 0
+            prevStreak = 0
+            streaks = []
+            streak = []
+            @users.each do |hsh|
+            #     currPurchaseDate = hsh[:eaten_at]
+            #     if prev != nil
+            #         if prevPurchaseDate == currPurchaseDate
+            #             currStreak+=1
+            #         else
+            #             if currStreak >= prevStreak
+            #                 streak.push(prevPurchaseDate)
+            #             else
+            #                 if streak.length > 1
+            #                     streaks.push(streak)
+            #                     streak = []
+            #                 end
+            #             end
+            #         end
+            #         prevPurchaseDate = currPurchaseDate
+            #     else
+            #         prevPurchaseDate = hsh[:eaten_at]
+            #     end
+            # end
+        end        
+    end
+
   end
 end
--- models/models.rb
+++ models/models.rb
@@ -4,6 +4,29 @@ require 'csv'
 # connect to local file database
 database = Sequel.postgres('pizzaAnalytics')
 
+# drop the tables if they exist
+database.run "DROP TABLE loader;"
+# database.run "drop table persons;"
+database.run "DROP TABLE people;"
+database.run "DROP TABLE pizzas;"
+# create the loader relation with csv file. this will contain all data, and we can 
+database.run "CREATE TABLE loader (name text not null, type text not null, date timestamp not null);"
+database.run "COPY loader(name, type, date) FROM '/Users/cjr/pizzaAPI/pizzaAnalytics/data.csv' delimiter ',' CSV HEADER;"
+# use loader to create persons table with unique id's
+# database.run "create table persons(name text not null);"
+database.run "CREATE TABLE people(name text not null);"
+# database.run "insert into persons(name) select distinct name from loader;"
+database.run "INSERT INTO people(name) SELECT DISTINCT name from loader;"
+# database.run "alter table persons add column id serial primary key;"
+database.run "ALTER TABLE people ADD COLUMN id serial primary key;"
+# use loader to create pizzas table
+database.run "CREATE TABLE pizzas(name text not null, type text not null, date timestamp not null);"
+database.run "INSERT INTO pizzas(name, type, date) SELECT * FROM loader;"
+database.run "ALTER TABLE pizzas ADD COLUMN person_id integer;"
+# database.run "UPDATE pizzas p2 SET person_id = p1.id FROM pizzas p1 WHERE p2.name = p1.name"
+database.run "UPDATE pizzas SET person_id = p.id FROM persons p WHERE p.name = pizzas.name"
+database.run "ALTER TABLE pizzas ADD COLUMN id serial primary key;"
+
 #
 # entity Person
 #
